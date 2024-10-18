06ab2c9cbb7f5ccf8a52ccad4dcb8b6cb61ba77c
d3fenddr <ggeasyggeasy79@gmail.com>
2024-10-19 01:29:15 +0400

    TCP_GuessNumber
    Add TCP_GuessNumber

---
+++ TCP_GuessNumber/ClientApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+const int serverPort = 7070;
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+using TcpClient tcpClient = new TcpClient();
+await tcpClient.ConnectAsync(ipAddress, serverPort);
+
+var tcpClientStream = tcpClient.GetStream();
+var streamReader = new StreamReader(tcpClientStream);
+var streamWriter = new StreamWriter(tcpClientStream) { AutoFlush = true }; // dla togo chtobi ne pisat vezde FlushAsync()
+
+Console.WriteLine("Connection established. Try to guess the number from 1 - 100. You have 5 attempts!");
+
+_ = Task.Run(async () =>
+{
+    while (true)
+    {
+        string? serverMessage = await streamReader.ReadLineAsync();
+        if (serverMessage != null)
+        {
+            Console.WriteLine(serverMessage);
+            if (serverMessage.Contains("Win") || serverMessage.Contains("lost"))
+            {
+                tcpClient.Close();
+                Environment.Exit(0); // close program with exit code 0
+            }
+        }
+    }
+});
+
+while (true)
+{
+    Thread.Sleep(100);
+    Console.Write("Enter the number: ");
+    string guess = Console.ReadLine();
+    await streamWriter.WriteLineAsync(guess);
+}
---
+++ TCP_GuessNumber/ServerApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+const int serverPort = 7070;
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+TcpListener tcpListener = new TcpListener(ipAddress, serverPort);
+
+tcpListener.Start();
+Console.WriteLine($"Server started on port {serverPort}...");
+
+while (true)
+{
+    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+    Console.WriteLine("Client connected!");
+
+    _ = Task.Run(async () =>
+    {
+        var tcpClientStream = tcpClient.GetStream();
+        var streamReader = new StreamReader(tcpClientStream);
+        var streamWriter = new StreamWriter(tcpClientStream) { AutoFlush = true };
+
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101);
+        int attemptsLeft = 5;
+        bool isGuessed = false;
+
+        while (attemptsLeft > 0 && !isGuessed)
+        {
+            try
+            {
+                string? clientMessage = await streamReader.ReadLineAsync();
+                if (clientMessage == null) break;
+
+                int guessedNumber;
+                if (int.TryParse(clientMessage, out guessedNumber))
+                {
+                    if (guessedNumber == secretNumber)
+                    {
+                        await streamWriter.WriteLineAsync("You guessed the number! Win!");
+                        isGuessed = true;
+                    }
+                    else if (guessedNumber > secretNumber)
+                    {
+                        await streamWriter.WriteLineAsync($"\nThe right number is lower. Attempts left: {--attemptsLeft}");
+                    }
+                    else
+                    {
+                        await streamWriter.WriteLineAsync($"\nThe right number is higher. Attempts left: {--attemptsLeft}");
+                    }
+                }
+                else
+                {
+                    await streamWriter.WriteLineAsync("Wrong insert. Please try again");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                break;
+            }
+        }
+
+        if (!isGuessed && attemptsLeft == 0)
+        {
+            await streamWriter.WriteLineAsync($"You lost! Correct number was: {secretNumber}");
+        }
+
+        tcpClient.Close();
+    });
+}
